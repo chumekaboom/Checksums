cab8492da0e0811d874070f861580525b3d33729
Charlie Hume <charliehume33@gmail.com>
2015-03-04 23:27:36 -0500

    Edited source names

--- appinfo/app.php
+++ appinfo/app.php
@@ -5,5 +5,5 @@
 // Jquery
 OCP\Util::addScript("3rdparty", "chosen/chosen.jquery.min");
 // checksum script
-OCP\Util::addScript('checksums', "checksum" );
+OCP\Util::addScript('checksums', "checksum_sha1" );
 OCP\Util::addScript('checksums', "checksum_md5" );
--- js/checksum_sha1.js
+++ js/checksum_sha1.js
@@ -1,22 +1,22 @@
 (function($){
-  var checksum = {
+  var checksum_sha1 = {
     init: function() {
       if (typeof FileActions !== 'undefined') {
         FileActions.register(
           'file',
-          t('checksum','checksum'),
+          t('checksum_sha1','checksum'),
           OC.PERMISSION_READ,
           OC.imagePath('core','actions/info'),
-          checksum.check
+          checksum_sha1.check
         );
       };
     },
     check: function(file) {
       dom = this.elem.find('.action[data-action=checksum]');
       if(!dom.hasClass('chcksum-hashed')) {
-        dom.html(checksum.load);
-        dom.addClass('checksum-hashing');
-        checksum.ajax(file);
+        dom.html(checksum_sha1.load);
+        dom.addClass('checksum_sha1-hashing');
+        checksum_sha1.ajax(file);
       } else {
         alert(dom.html());
       }
@@ -26,18 +26,18 @@
       var data = {source: file, dir: $('#dir').val()+'/'};
       $.ajax({
       type: 'GET',
-      url: OC.filePath('checksums', 'ajax', 'checksum.php'),
+      url: OC.filePath('checksums', 'ajax', 'checksum_sha1.php'),
       dataType: 'json',
       data: data,
       async: false,
       success: function(info) {
-        dom = $('.checksum-hashing').first();
+        dom = $('.checksum_sha1-hashing').first();
         dom.text('SHA1: '+info.data[0]);
         dom.addClass('chcksum-hashed');
-        dom.removeClass('checksum-hashing');
+        dom.removeClass('checksum_sha1-hashing');
       }
     });
     }
   }
-  $(document).ready(checksum.init);
+  $(document).ready(checksum_sha1.init);
 })(jQuery)
